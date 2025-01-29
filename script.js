02dd43cee0697350943ecc22f006a18fb485e339
5akurasi <5akurasilegends@gmail.com>
2025-01-29 13:28:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example: Smooth Scroll for internal links (if added)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
